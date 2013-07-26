13fb95433f18edd57ed09afa017d2b0dbeb6acd2
Erik Khalimov <biomaks@gmail.com>
2013-07-26 20:22:49 +0400

    added moving old log files

--- money3.sh
+++ money3.sh
@@ -8,7 +8,9 @@ LOGFILE=logs/money$CURDATE.log
 JA_PID=target/money3.pid
 PROG_BIN="mvn $PROGRAM"
 PID=`ps -aef | grep "$PROGRAM" | grep -v grep | awk '{print $2}'`
-#find logs/ -type f -mtime -1 -name "*.log*" -exec mv {} logs/old \;
+
+#move logs modified more than 24h ago to logs/old folder
+find logs/ -type f -mtime -1 -name "*.log*" -exec mv {} logs/old \;
 
 
 case "$1" in
@@ -16,25 +18,25 @@ case "$1" in
 
     if [ ! -z $PID ]
     then
-    echo -n " "
-    echo -n "Server money3 is running"
-    echo " "
+        echo -n " "
+        echo -n "Server money3 is running"
+        echo " "
     else
-    echo -n "Starting server money3: "
-    echo ""
-    $PROG_BIN > $LOGFILE 2>&1 &
-    echo $! > $JA_PID
-    echo "Server money3 started. PID:$!"
-    echo "Logfile is : $LOGFILE"
-    echo ""
+        echo -n "Starting server money3: "
+        echo ""
+        $PROG_BIN > $LOGFILE 2>&1 &
+        echo $! > $JA_PID
+        echo "Server money3 started. PID:$!"
+        echo "Logfile is : $LOGFILE"
+        echo ""
     fi
     ;;
   stop)
     echo -n "Shutting down money3 server: "
     echo ""
     for i in $PID; do
-    kill -9 $i
-    rm -f $JA_PID
+        kill -9 $i
+        rm -f $JA_PID
     done
     echo -n "Server money3 is down."
     echo ""
@@ -48,15 +50,15 @@ case "$1" in
     STATUS=`ps -aef | grep "$PROGRAM" | grep -v grep | awk '{print $2}' | head -1`
     if [ ! -z $STATUS ]
     then
-    echo -n "Server money3 is running"
-    echo ""
-    for i in $PID; do
-    echo PID:$i
-    done
-    echo ""
+        echo -n "Server money3 is running"
+        echo ""
+        for i in $PID; do
+            echo PID:$i
+        done
+        echo ""
     else
-    echo -n "Server money3 is down."
-    echo ""
+        echo -n "Server money3 is down."
+        echo ""
     fi
     ;;
   *)
