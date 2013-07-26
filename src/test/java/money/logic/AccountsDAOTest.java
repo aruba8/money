15f114d602d28f2cfbfa45b74c418f1e123d4242
Erik Khalimov <biomaks@gmail.com>
2013-07-26 19:57:52 +0400

    added test framework and emty tests

---
+++ src/test/java/money/logic/AccountsDAOTest.java
@@ -0,0 +1,12 @@
+package money.logic;
+
+import org.testng.annotations.BeforeClass;
+
+/**
+ * author: erik
+ */
+public class AccountsDAOTest {
+    @BeforeClass
+    public void setUp() {
+    }
+}
---
+++ src/test/java/money/routes/AccountRoutesTest.java
@@ -0,0 +1,12 @@
+package money.routes;
+
+import org.testng.annotations.BeforeClass;
+
+/**
+ * author: erik
+ */
+public class AccountRoutesTest {
+    @BeforeClass
+    public void setUp() {
+    }
+}
